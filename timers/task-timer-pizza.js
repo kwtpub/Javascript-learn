32dae15d2a70a5676b49aee40f7597f57469384f
kwtpub <skk1200@mail.ru>
2025-09-04 22:59:17 +0400

    done pixxa task

--- timers/task-timer-pizza.js
+++ timers/task-timer-pizza.js
@@ -1,12 +1,20 @@
 function pizzaTimer(time) {
-  let formatTimes = new Intl
+  const options = {
+    minute: "2-digit",
+    second: "2-digit"
+  }
+  let dateTime = (new Date()).setTime(time);  
+  let formatTimes = new Intl.DateTimeFormat("ru-RU", options).format(dateTime)
   const interval = setInterval(() => {
-    console.log(``)
+    console.log(formatTimes)
+    time = time - 1000
+    dateTime = (new Date()).setTime(time);
+    formatTimes = new Intl.DateTimeFormat("ru-RU", options).format(dateTime)
   }, 1000);
   setTimeout(() => {
     clearInterval(interval)
     console.log('!!!')
-  }, time);
+  }, time+500);
 }
 
-pizzaTimer(1000)
+pizzaTimer(8000)
